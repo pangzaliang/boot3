1b8d3a74d8121552f22872fb90464e1cc5980e4d
Zero <liangliangqingcheng@outlook.com>
2023-01-08 15:55:19 +0800

    提交代码

--- src/main/java/org/zero/boot3/service/UserService.java
+++ src/main/java/org/zero/boot3/service/UserService.java
@@ -1,9 +1,13 @@
 package org.zero.boot3.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public String getUserById(Integer id){
         System.out.println("getUserById(" + id +")......");
@@ -12,6 +16,7 @@ public class UserService {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+            logger.error("UserService getUserById InterruptedException = ", e);
         }
 
         return "giao";
